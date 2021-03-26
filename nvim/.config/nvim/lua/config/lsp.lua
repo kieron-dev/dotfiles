7a53250acbcfc6738c6734fa8b5dab65511c812c
Kieron Browne <kieron.browne@gmail.com>
2021-03-26 19:01:57 +0000

    Add lua LSP

--- nvim/.config/nvim/lua/config/lsp.lua
+++ nvim/.config/nvim/lua/config/lsp.lua
@@ -29,20 +29,6 @@ local on_attach = function(client, bufnr)
   elseif client.resolved_capabilities.document_range_formatting then
     buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
   end
-
-  -- -- Set autocommands conditional on server_capabilities
-  -- if client.resolved_capabilities.document_highlight then
-  --   vim.api.nvim_exec([[
-  --     hi LspReferenceRead cterm=bold ctermbg=red guibg=LightYellow
-  --     hi LspReferenceText cterm=bold ctermbg=red guibg=LightYellow
-  --     hi LspReferenceWrite cterm=bold ctermbg=red guibg=LightYellow
-  --     augroup lsp_document_highlight
-  --       autocmd! * <buffer>
-  --       autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-  --       autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-  --     augroup END
-  --   ]], false)
-  -- end
 end
 
 -- Use a loop to conveniently both setup defined servers
@@ -61,6 +47,37 @@ nvim_lsp.gopls.setup{
         gofumpt = true
     };
 }
+local sumneko_root_path = '/home/kieron/src/lua-language-server'
+local sumneko_binary = sumneko_root_path.."/bin/Linux/lua-language-server"
+
+require('lspconfig').sumneko_lua.setup({
+  cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
+  -- An example of settings for an LSP server.
+  --    For more options, see nvim-lspconfig
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+        -- Setup your lua path
+        path = vim.split(package.path, ';'),
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = {
+          [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+          [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+        },
+      },
+    }
+  },
+
+  on_attach = on_attach
+})
 
 require'lspconfig'.solargraph.setup {
     on_attach = on_attach;
